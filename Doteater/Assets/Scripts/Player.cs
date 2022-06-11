3a66cbaffc3bc16e446dc48ddc9485ca255fbf3a
송명석 <01audtjr@gmail.com>
2022-06-11 16:44:34 +0900

    0611
    doteater

---
+++ Doteater/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 360f;
+
+    CharacterController charCtrl;
+    Animator anim;
+
+    void Start()
+    {
+        charCtrl = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();//unitychanÇÁ¸®ÆÕÀÇ animator
+    }
+
+    void Update()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (dir.sqrMagnitude > 0.01f)
+        {
+            Vector3 forward = Vector3.Slerp(transform.forward, dir, rotationSpeed * Time.deltaTime / Vector3.Angle(transform.forward, dir));
+            transform.LookAt(transform.position + forward);
+        }
+        charCtrl.Move(dir * moveSpeed * Time.deltaTime);
+        anim.SetFloat("Speed", charCtrl.velocity.magnitude);
+        if (GameObject.FindGameObjectsWithTag("Dot").Length < 1)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch(other.tag)
+        {
+            case "Dot":
+                Destroy(other.gameObject);
+                break;
+            case "Enemy":
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                break;
+        }
+
+    }
+}
